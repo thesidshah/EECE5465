a00fc5319c5622744ecb77a5c7d802e6397ac4fe
Sid <siddhant997@gmail.com>
2023-02-06 10:23:15 -0500

    This is gonna end with an error =D

--- TextAnalyzer.py
+++ TextAnalyzer.py
@@ -113,6 +113,7 @@ if __name__ == "__main__":
     elif(args.mode == "DFF"):
         easy_words = create_list_from_file(args.simple_words)
         diff_words = count_difficult_words(lines, easy_words)
+        diff_words = diff_words.collect()
         print(diff_words)
 
     end = time()
