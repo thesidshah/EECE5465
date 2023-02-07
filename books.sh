7cfb913ba0aa5b464f870c8c2048be52f0d66ab5
Sid <siddhant997@gmail.com>
2023-02-07 18:16:06 -0500

    only committing books,sh

---
+++ books.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# For loop with individual numbers
+mkdir books
+for i in "Frankenstein.txt"  "JaneEyre.txt"  "MobyDick.txt"  "PrideAndPrejudice.txt"  "TheOdyssey.txt"  "TheStoryOfTheStone.txt"  "WarAndPeace.txt"
+do
+   python TextAnalyzer.py DCF  /work/courses/EECE5645/HW1/Data/Books/$i >>output.log_"$i"
+done
